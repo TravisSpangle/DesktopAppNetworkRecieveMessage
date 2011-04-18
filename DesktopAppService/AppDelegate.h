3c035dab6f89ae2dc99e6ce1599824886027bea3
Travis Spangle <tspangle@Travis-Spangles-MacBook.local>
2011-04-17 23:04:15 -0700

    Renamed DesktopAppServiceDelegate to AppDelegate.

---
+++ DesktopAppService/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  DesktopAppServiceAppDelegate.h
+//  DesktopAppService
+//
+//  Created by Travis Spangle on 4/17/11.
+//  Copyright 2011 Peak Systems. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate> {
+@private
+    NSWindow *window;
+    NSTextField *serverLog;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet NSTextField *serverLog;
+    
+@end
---
+++ DesktopAppService/AppDelegate.m
@@ -0,0 +1,21 @@
+//
+//  DesktopAppServiceAppDelegate.m
+//  DesktopAppService
+//
+//  Created by Travis Spangle on 4/17/11.
+//  Copyright 2011 Peak Systems. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window;
+@synthesize serverLog;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
